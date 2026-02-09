58a1e35193a0f2e65e567b296d043437d02848b8
amadeuzjason <amadeusgwh1705@gmail.com>
2026-02-09 08:18:51 +0800

    Fix PSR-4 autoloading case sensitivity issues

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\authController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\PdfController;
 use Illuminate\Support\Facades\DB;
@@ -14,9 +14,9 @@ use App\Http\Controllers\KunjunganController;
 Route::get('/debug-users', function() {
     return App\Models\User::all(['id', 'username', 'kode', 'role', 'is_approved']);
 });
-Route::post('/register', [authController::class, 'register']);
-Route::post('/register2', [authController::class, 'register2']);
-Route::post('/login', [authController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register2', [AuthController::class, 'register2']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/pdf/{filename}', [PdfController::class, 'serve'])->where('filename', '.*');
 
 // Public Book Routes
@@ -53,18 +53,18 @@ Route::get('/books-kelas-12/{id}', [BookController::class, 'getKelas12BookById']
 Route::get('/books/non-akademik/{id}', [BookController::class, 'getNonAkademikBookById']);
 
 Route::middleware('auth:sanctum')->group(function() {
-Route::get('/user',[authController::class, 'user']);
-Route::get('/siswa',[authController::class, 'siswa']);
-Route::get('/siswa-sd',[authController::class, 'sdSiswa']);
-Route::get('/siswa-smp',[authController::class, 'smpSiswa']);
-Route::get('/siswa-smk',[authController::class, 'smkSiswa']);
-Route::get('/guru',[authController::class, 'guru']);
-Route::get('/guru-sd',[authController::class, 'sdGuru']);
-Route::get('/guru-smp',[authController::class, 'smpGuru']);
-Route::get('/guru-smk',[authController::class, 'smkGuru']);
-Route::get('/perpus',[authController::class, 'perpus']);
-Route::post('/logout', [authController::class, 'logout']);
-Route::post('/change-password', [authController::class, 'changePassword']);
+Route::get('/user',[AuthController::class, 'user']);
+Route::get('/siswa',[AuthController::class, 'siswa']);
+Route::get('/siswa-sd',[AuthController::class, 'sdSiswa']);
+Route::get('/siswa-smp',[AuthController::class, 'smpSiswa']);
+Route::get('/siswa-smk',[AuthController::class, 'smkSiswa']);
+Route::get('/guru',[AuthController::class, 'guru']);
+Route::get('/guru-sd',[AuthController::class, 'sdGuru']);
+Route::get('/guru-smp',[AuthController::class, 'smpGuru']);
+Route::get('/guru-smk',[AuthController::class, 'smkGuru']);
+Route::get('/perpus',[AuthController::class, 'perpus']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/change-password', [AuthController::class, 'changePassword']);
 
 Route::get('/kunjungans', [KunjunganController::class, 'indexHariIni']);
 Route::get('/data-kunjungans', [KunjunganController::class, 'index']);
@@ -74,39 +74,39 @@ Route::get('/kunjungans/rekap2', [KunjunganController::class, 'rekap2']);
 
   
 
-Route::get('/users-pending', [authController::class, 'pendingUsers']);
-Route::post('/approve-user/{id}', [authController::class, 'approveUser']);
-Route::delete('/reject-user/{id}', [authController::class, 'rejectUser']);
+Route::get('/users-pending', [AuthController::class, 'pendingUsers']);
+Route::post('/approve-user/{id}', [AuthController::class, 'approveUser']);
+Route::delete('/reject-user/{id}', [AuthController::class, 'rejectUser']);
 
-Route::get('/siswa/{id}', [authController::class, 'getSiswa']);
-Route::get('/siswa-sd/{id}', [authController::class, 'getSdSiswa']);
-Route::get('/siswa-smp/{id}', [authController::class, 'getSmpSiswa']);
-Route::get('/siswa-smk/{id}', [authController::class, 'getSmkSiswa']);
-Route::get('/guru/{id}', [authController::class, 'getGuru']);
-Route::get('/guru-sd/{id}', [authController::class, 'getSdGuru']);
-Route::get('/guru-smp/{id}', [authController::class, 'getSmpGuru']);
-Route::get('/guru-smk/{id}', [authController::class, 'getSmkGuru']);
-Route::get('/perpus/{id}', [authController::class, 'getPerpus']);
-Route::delete('/delete/{id}', [authController::class, 'deleteUser']);
-Route::delete('/siswa/{id}', [authController::class, 'deleteSiswa']);
-Route::delete('/siswa-sd/{id}', [authController::class, 'deleteSdSiswa']);
-Route::delete('/siswa-smp/{id}', [authController::class, 'deleteSmpSiswa']);
-Route::delete('/siswa-smk/{id}', [authController::class, 'deleteSmkSiswa']);
-Route::delete('/guru/{id}', [authController::class, 'deleteGuru']);
-Route::delete('/guru-sd/{id}', [authController::class, 'deleteSdGuru']);
-Route::delete('/guru-smp/{id}', [authController::class, 'deleteSmpGuru']);
-Route::delete('/guru-smk/{id}', [authController::class, 'deleteSmkGuru']);
-Route::delete('/perpus/{id}', [authController::class, 'deletePerpus']);
-Route::post('/update/{id}', [authController::class, 'updateUser']);
-Route::post('/update-siswa/{id}',[authController::class, 'updateSiswa']);
-Route::post('/update-siswa-sd/{id}', [authController::class, 'updateSdSiswa']);
-Route::post('/update-siswa-smp/{id}', [authController::class, 'updateSmpSiswa']);
-Route::post('update-siswa-smk/{id}', [authController::class, 'updateSmkSiswa']);
-Route::post('/update-guru/{id}',[authController::class, 'updateGuru']);
-Route::post('/update-guru-sd/{id}', [authController::class, 'updateSdGuru']);
-Route::post('/update-guru-smp/{id}', [authController::class, 'updateSmpGuru']);
-Route::post('/update-guru-smk/{id}', [authController::class, 'updateSmkGuru']);
-Route::post('/update-perpus/{id}',[authController::class, 'updatePerpus']);
+Route::get('/siswa/{id}', [AuthController::class, 'getSiswa']);
+Route::get('/siswa-sd/{id}', [AuthController::class, 'getSdSiswa']);
+Route::get('/siswa-smp/{id}', [AuthController::class, 'getSmpSiswa']);
+Route::get('/siswa-smk/{id}', [AuthController::class, 'getSmkSiswa']);
+Route::get('/guru/{id}', [AuthController::class, 'getGuru']);
+Route::get('/guru-sd/{id}', [AuthController::class, 'getSdGuru']);
+Route::get('/guru-smp/{id}', [AuthController::class, 'getSmpGuru']);
+Route::get('/guru-smk/{id}', [AuthController::class, 'getSmkGuru']);
+Route::get('/perpus/{id}', [AuthController::class, 'getPerpus']);
+Route::delete('/delete/{id}', [AuthController::class, 'deleteUser']);
+Route::delete('/siswa/{id}', [AuthController::class, 'deleteSiswa']);
+Route::delete('/siswa-sd/{id}', [AuthController::class, 'deleteSdSiswa']);
+Route::delete('/siswa-smp/{id}', [AuthController::class, 'deleteSmpSiswa']);
+Route::delete('/siswa-smk/{id}', [AuthController::class, 'deleteSmkSiswa']);
+Route::delete('/guru/{id}', [AuthController::class, 'deleteGuru']);
+Route::delete('/guru-sd/{id}', [AuthController::class, 'deleteSdGuru']);
+Route::delete('/guru-smp/{id}', [AuthController::class, 'deleteSmpGuru']);
+Route::delete('/guru-smk/{id}', [AuthController::class, 'deleteSmkGuru']);
+Route::delete('/perpus/{id}', [AuthController::class, 'deletePerpus']);
+Route::post('/update/{id}', [AuthController::class, 'updateUser']);
+Route::post('/update-siswa/{id}',[AuthController::class, 'updateSiswa']);
+Route::post('/update-siswa-sd/{id}', [AuthController::class, 'updateSdSiswa']);
+Route::post('/update-siswa-smp/{id}', [AuthController::class, 'updateSmpSiswa']);
+Route::post('update-siswa-smk/{id}', [AuthController::class, 'updateSmkSiswa']);
+Route::post('/update-guru/{id}',[AuthController::class, 'updateGuru']);
+Route::post('/update-guru-sd/{id}', [AuthController::class, 'updateSdGuru']);
+Route::post('/update-guru-smp/{id}', [AuthController::class, 'updateSmpGuru']);
+Route::post('/update-guru-smk/{id}', [AuthController::class, 'updateSmkGuru']);
+Route::post('/update-perpus/{id}',[AuthController::class, 'updatePerpus']);
 
 
 
@@ -160,4 +160,3 @@ Route::delete('/books-kelas-11/{id}', [BookController::class, 'deleteKelas11Book
 Route::delete('/books-kelas-12/{id}', [BookController::class, 'deleteKelas12Book']);
 Route::delete('/books-non-akademik/{id}', [BookController::class, 'deleteNonAkademikBook']);
 });
-
